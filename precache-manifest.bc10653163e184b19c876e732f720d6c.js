e0b13e845e578f54cb72224c73baa7b10baa51e4
Ron <rongalron@gmail.com>
2020-05-26 23:48:52 +0300

    Updates

---
+++ precache-manifest.bc10653163e184b19c876e732f720d6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79be380b68e86f46b41b44b5ce0fdc31",
+    "url": "/roboFriend/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/roboFriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e98ed9beca1dc98d0f40",
+    "url": "/roboFriend/static/css/main.a937e291.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/roboFriend/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriend/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98ed9beca1dc98d0f40",
+    "url": "/roboFriend/static/js/main.b7e292e0.chunk.js"
+  },
+  {
+    "revision": "fc0d15d413cddf17d4c0",
+    "url": "/roboFriend/static/js/runtime-main.933f1051.js"
+  },
+  {
+    "revision": "76228bb6cfefd86926ab8edd2167b645",
+    "url": "/roboFriend/static/media/Resident Evil Large.76228bb6.woff"
+  }
+]);
