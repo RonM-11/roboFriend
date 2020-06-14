98c103c4c4420a63d314f120574c7fbeb84f87cc
Ron <rongalron@gmail.com>
2020-06-14 21:11:27 +0300

    redux + style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './card.css';
 
 const Card = ({name, email, id}) =>{
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className='card tc dib br3 pa3 ma2 grow bw2 shadow-5' >
 			<img alt= 'robots' src= {`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
